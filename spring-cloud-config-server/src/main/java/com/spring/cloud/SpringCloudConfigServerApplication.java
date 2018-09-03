c3374f37e5d88a544bd45ce10ebfde0331623fb9
zhuoyuexin <zhuoyuexin@21cn.com>
2018-09-03 12:11:15 +0800

    服务提供者

---
+++ spring-cloud-config-server/src/main/java/com/spring/cloud/SpringCloudConfigServerApplication.java
@@ -0,0 +1,16 @@
+package com.spring.cloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@EnableConfigServer
+@SpringBootApplication
+public class SpringCloudConfigServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringCloudConfigServerApplication.class, args);
+    }
+}
